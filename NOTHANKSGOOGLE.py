066d4b3d9679e63dafac43c53c31499a8975caea
Justin Mayo <41157020+jjustinmayo@users.noreply.github.com>
2023-03-24 17:26:48 -0400

    NO THANKS GOOGLE

--- NOTHANKSGOOGLE.py
+++ NOTHANKSGOOGLE.py
@@ -1,10 +1,5 @@
-# import a module that generates random characters whenever "random." is used 
 import random
 
-# Create variables for each random character 
-# the "random.randint()" method returns an integer from the specified range. The range used for  each variable is based on unicode (ASCII) values
-# "chr()" TO STUDY
-# "str()" TO STUDY
 lowercaseLetter1 = chr(random.randint(97,122))   
 lowercaseLetter2 = chr(random.randint(97,122))  
 uppercaseLetter1 = chr(random.randint(65,90))
@@ -15,14 +10,11 @@ punctuation1 = chr(random.randint(33,47))
 punctuation2 = chr(random.randint(33,47))
 
 
-def pwGenerator():
-    # create a new list which lists the objects as strings in order
+def nothanksGoogle():
     pwList = [lowercaseLetter1, lowercaseLetter2, uppercaseLetter1, uppercaseLetter2, digit1, digit2, punctuation1, punctuation2]
-    # using the random module, use .shuffle to randomize the order of each object in the list created
     random.shuffle(pwList)
-    # to STUDY
     randomPw = "".join(map(str,pwList))
     print(randomPw)
     
-pwGenerator()
+nothanksGoogle()
 
